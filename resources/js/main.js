473fee3b9fb2a69a24e4510633d53d35a35f3688
AinDanisya16 <155231062+AinDanisya16@users.noreply.github.com>
2025-07-01 13:50:43 +0800

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './assets/main.css'
 
 const app = createApp(App)
 const pinia = createPinia()
