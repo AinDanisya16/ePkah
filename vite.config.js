a32877bb04d940e1a4fb2860ca397d63b3840937
AinDanisya16 <155231062+AinDanisya16@users.noreply.github.com>
2025-07-01 14:06:25 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'public/dist',
+    outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       input: 'resources/js/main.js',
